b04d4640826868fab4ea61a2f87fd96482d22875
Anthony Sims <anthonysims50@yahoo.com>
2023-03-17 20:04:54 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `  Anthony's Gatsby Website`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
